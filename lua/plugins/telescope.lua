82fc14fa6df401435642e8143fc5fda876ad1def
Alberto Russo <alberto@albertorusso.it>
2023-10-06 10:18:00 +0200

    telescope search hidden file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,13 +12,27 @@ return {
     -- change some options
     opts = {
       defaults = {
-        hidden = true,
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+        },
         path_display = {
           shorten = {
             len = 3,
             exclude = { 1, -1 },
           },
           truncate = true,
+        },
+      },
+      pickers = {
+        find_files = {
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
         },
       },
     },
