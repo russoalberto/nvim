f78e3fffa5319b9c5483dfb72d7111d73ff0392b
Alberto Russo <alberto@albertorusso.it>
2023-10-28 16:54:44 +0200

    feat: clean and update git plugins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,36 +1,14 @@
 return {
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
   {
     'lewis6991/gitsigns.nvim',
     lazy = false,
     config = function()
       require('gitsigns').setup {}
     end,
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-      },
-      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-      end,
-    },
+  },
+  {
+    'f-person/git-blame.nvim',
+    event = 'VeryLazy',
   },
   {
     'kdheepak/lazygit.nvim',
