8cd3c52ccc35c80ed8cd2ed772392b05935fc93b
Alberto Russo <alberto@albertorusso.it>
2023-12-21 17:28:25 +0100

    fix: update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,10 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-      config = function()
-        require('telescope').load_extension 'fzf'
-      end,
     },
-    -- change some options
     opts = {
       defaults = {
         vimgrep_arguments = {
