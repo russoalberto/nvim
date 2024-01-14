bb0f40e2662f9dacbadcae17e4d26d23c7401053
Alberto Russo <alberto@albertorusso.it>
2024-01-14 21:57:50 +0100

    feat: update lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,85 +1,22 @@
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(client, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-
-  if client.supports_method 'textDocument/formatting' then
-    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = augroup,
-      buffer = bufnr,
-      -- on 0.8, you should use vim.lsp.buf.format instead
-      callback = function()
-        vim.lsp.buf.format {}
-      end,
-    })
-  end
-end
-
 local servers = {
-  -- pyright will be automatically installed with mason and loaded with lspconfig
-  pyright = {},
   -- fontend
   angularls = {},
   cssls = {},
   tsserver = {},
   html = {
-    settings = {
-      html = {
-        format = {
-          tabSize = 2,
-          useTabs = false,
-          wrapAttributesIndentSize = 2,
-          wrapAttributes = 'preserve',
-          wrapLineLength = 0,
-        },
+    html = {
+      format = {
+        tabSize = 2,
+        useTabs = false,
+        wrapAttributesIndentSize = 2,
+        wrapAttributes = 'preserve',
+        wrapLineLength = 0,
       },
     },
   },
   tailwindcss = {},
 
   -- backend
-  csharp_ls = {},
   intelephense = {},
   gopls = {},
   omnisharp = {},
@@ -134,7 +71,6 @@ return {
 
       -- Ensure the servers above are installed
       local mason_lspconfig = require 'mason-lspconfig'
-
       mason_lspconfig.setup {
         ensure_installed = vim.tbl_keys(servers),
       }
@@ -143,11 +79,7 @@ return {
         function(server_name)
           require('lspconfig')[server_name].setup {
             capabilities = capabilities,
-            on_attach = on_attach,
-            settings = (servers[server_name] or {}).settings,
-            filetypes = (servers[server_name] or {}).filetypes,
-            workspace = (servers[server_name] or {}).workspace,
-            telemetry = (servers[server_name] or {}).telemetry,
+            settings = (servers[server_name] or {}),
           }
         end,
       }
