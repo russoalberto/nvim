979547b908d713b4504d3e7e3206e46bc40f02c6
Alberto Russo <alberto@albertorusso.it>
2024-03-18 11:35:26 +0100

    feat: add dab config

---
+++ lua/plugins/dab.lua
@@ -0,0 +1,41 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+    "Tastyep/structlog.nvim",
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+    dapui.setup()
+    require("mason").setup()
+
+    require('mason-nvim-dap').setup {
+      ensure_installed = {
+        "codelldb",
+        "coreclr",
+        "delve",
+      },
+      handlers = {},
+    }
+
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = '[D]ebug: Toggle [B]reakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = '[D]ebug: Set [B]reakpoint' })
+
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -56,8 +56,6 @@ return {
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = '[S]earch [M]arks' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     end,
   },
