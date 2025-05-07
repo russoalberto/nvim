52409b9bb7b9d25c233b3aed7e43167ca7eba781
Alberto Russo <a.russo@geckosoft.it>
2025-05-07 21:55:57 +0200

    feat: add treesitter languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
       require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
         ensure_installed = {
+          'angular',
           'astro',
           'bash',
           'c_sharp',
@@ -25,6 +26,7 @@ return {
           'scss',
           'tsx',
           'typescript',
+          'twig',
           'yaml',
         },
         highlight = {
